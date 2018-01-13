e65236246d011dfe40337b114168a5028fc3a9af
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-01-13 12:24:40 -0600

    initial firebase connection

---
+++ app_firebase.js
@@ -0,0 +1,13 @@
+ // Global vars and firebase set up
+var config = {
+    apiKey: "AIzaSyC443Ww585g_rt44P6U4Wd0FM35APRuh2w",
+    authDomain: "in-season-8f60b.firebaseapp.com",
+    databaseURL: "https://in-season-8f60b.firebaseio.com",
+    projectId: "in-season-8f60b",
+    storageBucket: "in-season-8f60b.appspot.com",
+    messagingSenderId: "865498629942"
+  };
+
+  firebase.initializeApp(config);
+
+
