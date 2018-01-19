b5f973d460c95c5ccd0d86c548992593fcec19dc
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-18 20:36:03 -0600

    adding weather onto dashboard given GL location

--- assets/javascript/geolocation.js
+++ assets/javascript/geolocation.js
@@ -1,3 +1,15 @@
+//Working on On Click (User logs in and go to dashboard)
+$("#login").on("click", function(){
+    if($("#username").val() != "" && $("#password").val() != ""){
+        location.href ="dashboard.html";
+    } else {
+        console.log('Invalid Entry');
+    }
+    console.log($("#password").val());
+})
+
+var yourCity;
+
 // Using HTML GEOLOCATION API to grab user location 
 
 function getLocation() {
@@ -8,10 +20,34 @@ function getLocation() {
     }
 }
 
+
 function showPosition(position) {
    console.log( "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude);
+    var APIKey = "739b19c2f23e5d1f4b6dc5cd5bbed8a3";
+    var zipCode = 60616;
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + zipCode + ",us&units=imperial&appid=" + APIKey; 
+    var queryURL ="https://api.openweathermap.org/data/2.5/weather?" + "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + APIKey; 
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    
+    .done(function(response) {
+        console.log(queryURL);
+        console.log(response);
+    
+        $("#weatherHere").html("<h3>" + Math.round(response.main.temp*(9/5) -459.67) + " fahrenheit for your city " + response.name + "</h3>");
+        yourCity= response.name;
+        console.log(yourCity);
+        
+    })
 }
 
 
 getLocation();
+
+
+
+
