c2264e342a5ce2f3f8f54b5623dab46a9a670464
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-20 09:59:10 -0600

    getting working queryUrl

--- seatgeekapi.js
+++ seatgeekapi.js
@@ -5,7 +5,7 @@ function getEvent() {
     var zipcode = 60646;
     console.log(zipcode);
     var key = '79ca89a731467d450c77440a9935a1d70598fb17021b4a3101d6aa43da268231';
-    var queryURL = 'https://api.seatgeek.com/2/events?postal_code=' + zipcode + '&api_key=79ca89a731467d450c77440a9935a1d70598fb17021b4a3101d6aa43da268231'
+    var queryURL = 'https://api.seatgeek.com/2/events?postal_code=' + zipcode + '&client_id=' + client_id + '&client_secret=' + client_secret;
 
 
     $.ajax({ url: queryUTL, method: 'GET' }).done(function(response) {})
