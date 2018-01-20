1596b412f3f512d45f2a4fd4869ac946a2fe29ba
jennyannewhite <30728980+jennyannewhite@users.noreply.github.com>
2018-01-20 10:22:41 -0600

    Add files via upload

--- assets/javascript/seatgeekapi.js
+++ assets/javascript/seatgeekapi.js
@@ -6,7 +6,7 @@ function getEvent() {
     console.log(zipcode);
     var client_id = 'MTAyNzcyNTl8MTUxNjMyNTU2Ni44Ng';
     var client_secret = '79ca89a731467d450c77440a9935a1d70598fb17021b4a3101d6aa43da268231';
-    var queryURL = 'https://api.seatgeek.com/2/events?postal_code=' + zipcode + '&client_id=' + client_id + '&client_secret' + client_secret;
+    var queryURL = 'https://api.seatgeek.com/2/events?postal_code=' + zipcode + '&client_id=' + client_id + '&client_secret=' + client_secret;
 
 
     $.ajax({ url: queryURL, method: 'GET' }).done(function(response) {})
